f17c8ab244aa0d94d4478b456c72df9efac73a21
Ayoubon <aarrasseayoub@gmail.com>
2023-12-10 22:32:13 +0100

    adding the create command for both file and dir

--- hdfs_cli/src/main.rs
+++ hdfs_cli/src/main.rs
@@ -1,5 +1,6 @@
 use clap::{App, Arg, SubCommand};
 use reqwest::Client;
+use serde_json::json;
 use tokio;
 
 #[tokio::main]
@@ -8,48 +9,38 @@ async fn main() {
         .version("1.0")
         .author("Your Name")
         .about("Performs HTTP requests")
-        .subcommand(SubCommand::with_name("get")
-            .about("performs a GET request")
-            .arg(Arg::with_name("url").required(true)))
-        .subcommand(SubCommand::with_name("post")
-            .about("performs a POST request")
-            .arg(Arg::with_name("url").required(true))
-            .arg(Arg::with_name("body").required(true)))
-        // Add similar subcommands for DELETE and PUT
-        .subcommand(SubCommand::with_name("delete")
-            .about("performs a DELETE request")
-            .arg(Arg::with_name("url").required(true)))
-        .subcommand(SubCommand::with_name("put")
-            .about("performs a PUT request")
-            .arg(Arg::with_name("url").required(true))
-            .arg(Arg::with_name("body").required(true)))
+        .subcommand(SubCommand::with_name("create")
+            .about("Creates a file or directory with the specified path")
+            .arg(Arg::with_name("path")
+                .required(true)
+                .index(1))
+            .arg(Arg::with_name("directory")
+                .short('d')
+                .long("directory")
+                .help("Create a directory instead of a file")))
+        // Add other subcommands as needed
         .get_matches();
 
     let client = Client::new();
+    let base_url = "http://localhost:8080";
 
     match matches.subcommand() {
-        Some(("get", get_matches)) => {
-            let url = get_matches.value_of("url").unwrap();
-            let response = client.get(url).send().await.unwrap();
+        Some(("create", create_matches)) => {
+            let path = create_matches.value_of("path").unwrap();
+
+            let (endpoint, key) = if create_matches.is_present("directory") {
+                ("createDir", "dirPath")
+            } else {
+                ("createFile", "filePath")
+            };
+        
+            let post_body = json!({ key: path });
+        
+            let url = format!("{}/{}", base_url, endpoint);
+            let response = client.post(&url).json(&post_body).send().await.unwrap();
             println!("Response: {:?}", response.text().await.unwrap());
         }
-        Some(("post", post_matches)) => {
-            let url = post_matches.value_of("url").unwrap();
-            let body = post_matches.value_of("body").unwrap();
-            let response = client.post(url).body(body.to_string()).send().await.unwrap();
-            println!("Response: {:?}", response.text().await.unwrap());
-        }
-        Some(("delete", delete_matches)) => {
-            let url = delete_matches.value_of("url").unwrap();
-            let response = client.delete(url).send().await.unwrap();
-            println!("Response: {:?}", response.text().await.unwrap());
-        }
-        Some(("put", put_matches)) => {
-            let url = put_matches.value_of("url").unwrap();
-            let body = put_matches.value_of("body").unwrap();
-            let response = client.put(url).body(body.to_string()).send().await.unwrap();
-            println!("Response: {:?}", response.text().await.unwrap());
-        }
+        // Handle other subcommands...
         _ => {
             println!("Invalid command");
         }
