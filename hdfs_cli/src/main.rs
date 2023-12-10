336cf2b72b9566c8f82c4a795a38b6731cb955b3
Ayoubon <aarrasseayoub@gmail.com>
2023-12-10 22:41:11 +0100

    code refactoring

--- hdfs_cli/src/main.rs
+++ hdfs_cli/src/main.rs
@@ -5,7 +5,20 @@ use tokio;
 
 #[tokio::main]
 async fn main() {
-    let matches = App::new("Rust CLI for REST Requests")
+    let matches = build_cli_app().get_matches();
+
+    let client = Client::new();
+    let base_url = "http://localhost:8080";
+
+    if let Some(("create", create_matches)) = matches.subcommand() {
+        handle_create_subcommand(create_matches, &client, base_url).await;
+    } else {
+        println!("Invalid command");
+    }
+}
+
+fn build_cli_app() -> App<'static> {
+    App::new("Rust CLI for REST Requests")
         .version("1.0")
         .author("Your Name")
         .about("Performs HTTP requests")
@@ -18,31 +31,20 @@ async fn main() {
                 .short('d')
                 .long("directory")
                 .help("Create a directory instead of a file")))
-        // Add other subcommands as needed
-        .get_matches();
+}
 
-    let client = Client::new();
-    let base_url = "http://localhost:8080";
+async fn handle_create_subcommand(create_matches: &clap::ArgMatches, client: &Client, base_url: &str) {
+    let path = create_matches.value_of("path").expect("Required argument missing: path");
 
-    match matches.subcommand() {
-        Some(("create", create_matches)) => {
-            let path = create_matches.value_of("path").unwrap();
+    let (endpoint, key) = if create_matches.is_present("directory") {
+        ("createDir", "dirPath")
+    } else {
+        ("createFile", "filePath")
+    };
 
-            let (endpoint, key) = if create_matches.is_present("directory") {
-                ("createDir", "dirPath")
-            } else {
-                ("createFile", "filePath")
-            };
-        
-            let post_body = json!({ key: path });
-        
-            let url = format!("{}/{}", base_url, endpoint);
-            let response = client.post(&url).json(&post_body).send().await.unwrap();
-            println!("Response: {:?}", response.text().await.unwrap());
-        }
-        // Handle other subcommands...
-        _ => {
-            println!("Invalid command");
-        }
-    }
+    let post_body = json!({ key: path });
+
+    let url = format!("{}/{}", base_url, endpoint);
+    let response = client.post(&url).json(&post_body).send().await.expect("Failed to send request");
+    println!("Response: {:?}", response.text().await.expect("Failed to read response"));
 }
