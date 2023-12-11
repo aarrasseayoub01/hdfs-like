7457c8e4b73f9f2d81c52eb17843aaafb0a74040
Ayoubon <aarrasseayoub@gmail.com>
2023-12-11 14:13:39 +0100

    cli -- read file metadata

--- hdfs_cli/src/main.rs
+++ hdfs_cli/src/main.rs
@@ -1,5 +1,7 @@
 use clap::{App, Arg, SubCommand};
 use reqwest::Client;
+use serde::{Deserialize, Serialize};
+use serde_json;
 use serde_json::json;
 use tokio;
 
@@ -12,6 +14,8 @@ async fn main() {
 
     if let Some(("create", create_matches)) = matches.subcommand() {
         handle_create_subcommand(create_matches, &client, base_url).await;
+    } else if let Some(("read", read_matches)) = matches.subcommand() {
+        handle_read_subcommand(read_matches, &client, base_url).await;
     } else {
         println!("Invalid command");
     }
@@ -22,19 +26,38 @@ fn build_cli_app() -> App<'static> {
         .version("1.0")
         .author("Your Name")
         .about("Performs HTTP requests")
-        .subcommand(SubCommand::with_name("create")
-            .about("Creates a file or directory with the specified path")
-            .arg(Arg::with_name("path")
-                .required(true)
-                .index(1))
-            .arg(Arg::with_name("directory")
-                .short('d')
-                .long("directory")
-                .help("Create a directory instead of a file")))
+        .subcommand(
+            SubCommand::with_name("create")
+                .about("Creates a file or directory with the specified path")
+                .arg(Arg::with_name("path").required(true).index(1))
+                .arg(
+                    Arg::with_name("directory")
+                        .short('d')
+                        .long("directory")
+                        .help("Create a directory instead of a file"),
+                ),
+        )
+        .subcommand(
+            SubCommand::with_name("read")
+                .about("Reads a file or directory with the specified path")
+                .arg(Arg::with_name("path").required(true).index(1))
+                .arg(
+                    Arg::with_name("directory")
+                        .short('d')
+                        .long("directory")
+                        .help("Read a directory instead of a file"),
+                ),
+        )
 }
 
-async fn handle_create_subcommand(create_matches: &clap::ArgMatches, client: &Client, base_url: &str) {
-    let path = create_matches.value_of("path").expect("Required argument missing: path");
+async fn handle_create_subcommand(
+    create_matches: &clap::ArgMatches,
+    client: &Client,
+    base_url: &str,
+) {
+    let path = create_matches
+        .value_of("path")
+        .expect("Required argument missing: path");
 
     let (endpoint, key) = if create_matches.is_present("directory") {
         ("createDir", "dirPath")
@@ -45,6 +68,58 @@ async fn handle_create_subcommand(create_matches: &clap::ArgMatches, client: &Cl
     let post_body = json!({ key: path });
 
     let url = format!("{}/{}", base_url, endpoint);
-    let response = client.post(&url).json(&post_body).send().await.expect("Failed to send request");
-    println!("Response: {:?}", response.text().await.expect("Failed to read response"));
+    let response = client
+        .post(&url)
+        .json(&post_body)
+        .send()
+        .await
+        .expect("Failed to send request");
+    println!(
+        "Response: {:?}",
+        response.text().await.expect("Failed to read response")
+    );
+}
+
+// Define a struct matching your JSON structure
+#[derive(Debug, Serialize, Deserialize)]
+struct FileData {
+    #[serde(rename = "ID")]
+    id: u64,
+    #[serde(rename = "Name")]
+    name: String,
+    #[serde(rename = "IsDir")]
+    is_dir: bool,
+    #[serde(rename = "Size")]
+    size: u64,
+    #[serde(rename = "Blocks")]
+    blocks: Option<Vec<u64>>, // adjust the type according to your actual data
+    #[serde(rename = "Timestamp")]
+    timestamp: String,
+}
+
+async fn handle_read_subcommand(read_matches: &clap::ArgMatches, client: &Client, base_url: &str) {
+    let path = read_matches
+        .value_of("path")
+        .expect("Required argument missing: path");
+
+    let (endpoint, key) = if read_matches.is_present("directory") {
+        ("readDir", "dirPath")
+    } else {
+        ("readFile", "filePath")
+    };
+
+    let url = format!("{}/{}?{}={}", base_url, endpoint, key, path);
+    let response = client
+        .get(&url)
+        .send()
+        .await
+        .expect("Failed to send request");
+
+    let response_text = response.text().await.expect("Failed to read response");
+
+    // Parse the response text into the FileData struct
+    let file_data: FileData = serde_json::from_str(&response_text).expect("Failed to parse JSON");
+
+    // Print the parsed data
+    println!("Parsed Response: {:?}", file_data);
 }
